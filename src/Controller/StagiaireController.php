496d70a3f5a3c2fd03e148a2a800eb155529f11c
davinahouquet <davina.houquet@hotmail.com>
2023-09-21 16:08:20 +0200

    Données stagiaires

---
+++ src/Controller/StagiaireController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\StagiaireRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class StagiaireController extends AbstractController
+{
+    #[Route('/stagiaire', name: 'app_stagiaire')]
+    public function index(StagiaireRepository $stagiaireRepository): Response
+    {
+
+        $stagiaires = $stagiaireRepository->findAll();
+
+        return $this->render('stagiaire/index.html.twig', [
+            'stagiaires' => $stagiaires
+        ]);
+    }
+}
