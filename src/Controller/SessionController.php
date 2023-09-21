07333f8accd128353cb3b850e56057d7a4aca835
davinahouquet <davina.houquet@hotmail.com>
2023-09-21 16:08:27 +0200

    Données session

---
+++ src/Controller/SessionController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\SessionRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class SessionController extends AbstractController
+{
+    #[Route('/session', name: 'app_session')]
+    public function index(SessionRepository $sessionRepository): Response
+    {
+        
+        $sessions = $sessionRepository->findAll();
+
+        return $this->render('session/index.html.twig', [
+            'sessions' => $sessions
+        ]);
+    }
+}
