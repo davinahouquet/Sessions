3f4619fdc11b6a5fca018f8a1b21e5bc6c6ae592
davinahouquet <davina.houquet@hotmail.fr>
2023-09-22 09:45:50 +0200

    Affichage des stagiaires inscrits à une session

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\Session;
 use App\Repository\SessionRepository;
 use Symfony\Component\HttpFoundation\Response;
-// use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
