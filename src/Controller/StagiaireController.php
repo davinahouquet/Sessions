20af3b22259f7086fa50830c386a7cb579f77431
davinahouquet <davina.houquet@hotmail.fr>
2023-09-23 20:48:41 +0200

    CRUD de l'entité Stagiaire

--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -3,30 +3,71 @@
 namespace App\Controller;
 
 use App\Entity\Stagiaire;
+use App\Form\StagiaireType;
 use App\Repository\StagiaireRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StagiaireController extends AbstractController
 {
-    #[Route('/stagiaire', name: 'app_stagiaire')]
-    public function index(StagiaireRepository $stagiaireRepository): Response
+    #[Route('/stagiaire/new', name: 'new_stagiaire')]
+    #[Route('/stagiaire/{id}/edit', name: 'edit_stagiaire')]
+    public function new_edit(Stagiaire $stagiaire = null, Request $request, EntityManagerInterface $entityManager): Response
     {
+        $isNew = !$stagiaire;
 
-        $stagiaires = $stagiaireRepository->findAll();
+        if ($isNew) {
+            $stagiaire = new Stagiaire();
+        }
+    
+        $form = $this->createForm(StagiaireType::class, $stagiaire);
+    
+        $form->handleRequest($request);
+    
+        if ($form->isSubmitted() && $form->isValid()) {
+    
+            $stagiaire = $form->getData();
+               
+            $entityManager->persist($stagiaire);
+            
+            $entityManager->flush();
+    
+            return $this->redirectToRoute('show_stagiaire', ['id' => $stagiaire->getId()]);
+        }
+    
+        return $this->render('stagiaire/new.html.twig', [
+            'formAddStagiaire' => $form,
+            'edit' => $stagiaire->getId()
+            ]);
+        }
 
-        return $this->render('stagiaire/index.html.twig', [
-            'stagiaires' => $stagiaires
-        ]);
+    #[Route('/stagiaire/{id}/delete', name: 'delete_stagiaire')]
+    public function delete(Stagiaire $stagiaire = null, EntityManagerInterface $entityManager)
+    {
+        $entityManager->remove($stagiaire);
+        $entityManager->flush();
+            
+        return $this->redirectToRoute(('app_stagiaire'));
     }
 
     #[Route('/stagiaire/{id}', name: 'show_stagiaire')]
     public function show(Stagiaire $stagiaire): Response
     {
-
         return $this->render('stagiaire/show.html.twig', [
             'stagiaire' =>  $stagiaire
+        ]);
+    }
+
+    #[Route('/stagiaire', name: 'app_stagiaire')]
+    public function index(StagiaireRepository $stagiaireRepository): Response
+    {
+        $stagiaires = $stagiaireRepository->findAll();
+
+        return $this->render('stagiaire/index.html.twig', [
+            'stagiaires' => $stagiaires
         ]);
     }
 }
---
+++ src/Form/StagiaireType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Stagiaire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class StagiaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('sexe', ChoiceType::class, [
+                'choices'  => [
+                    'H' => true,
+                    'F' => false,
+                ],
+                ])
+            ->add('dateNaissance',DateType::class)
+            ->add('ville', TextType::class)
+            ->add('courriel', EmailType::class)
+            ->add('telephone', NumberType::class)
+            // ->add('sessions')
+            ->add('valider', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-success'
+                ]
+                ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Stagiaire::class,
+        ]);
+    }
+}
