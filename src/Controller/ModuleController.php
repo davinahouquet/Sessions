9bf1d906f6287425dfed8f94862a04cc9f259deb
davinahouquet <davina.houquet@hotmail.fr>
2023-09-23 21:39:08 +0200

    CRUD de l'entité Module

--- src/Controller/ModuleController.php
+++ src/Controller/ModuleController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Module;
+use App\Form\ModuleType;
 use App\Repository\ModuleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,11 +24,51 @@ class ModuleController extends AbstractController
         ]);
     }
 
+    #[Route('/module/new', name: 'new_module')]
+    #[Route('/module/{id}/edit', name: 'edit_module')]
+    public function new_edit(Module $module = null, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $isNew = !$module;
+
+        if ($isNew) {
+            $module = new Module();
+        }
+    
+        $form = $this->createForm(ModuleType::class, $module);
+    
+        $form->handleRequest($request);
+    
+        if ($form->isSubmitted() && $form->isValid()) {
+    
+            $module = $form->getData();
+               
+            $entityManager->persist($module);
+            
+            $entityManager->flush();
+    
+            return $this->redirectToRoute('show_module', ['id' => $module->getId()]);
+        }
+    
+        return $this->render('module/new.html.twig', [
+            'formAddModule' => $form,
+            'edit' => $module->getId()
+            ]);
+        }
+
     #[Route('/module/{id}', name: 'show_module')]
     public function show(Module $module): Response
     {
         return $this->render('module/show.html.twig', [
             'module' =>  $module
         ]);
+    }
+
+    #[Route('/module/{id}/delete', name: 'delete_module')]
+    public function delete(Module $module = null, EntityManagerInterface $entityManager)
+    {
+        $entityManager->remove($module);
+        $entityManager->flush();
+
+        return $this->redirectToRoute(('app_module'));
     }
 }
