bb5162ae40a82028c98241b96a0b697e86f05d47
davinahouquet <davina.houquet@hotmail.fr>
2023-10-01 21:52:53 +0200

    Dark/light mode

--- public/js/script.js
+++ public/js/script.js
@@ -13,7 +13,35 @@ links.forEach((link)=>{
 
 // Theme color : dark/light
 
-function myFunction() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
-}
+// Fonction simple SANS localsotrage
+// function myFunction() {
+//   var element = document.body;
+//   element.classList.toggle("dark-mode");
+// }
+
+// Le localstorage permet de conserver le thème choisi pendant qu'on navigue entre les vues
+function toggleDarkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    
+    // Vérifie si le mode sombre est activé
+    var isDarkMode = element.classList.contains("dark-mode");
+    
+    // Stocke l'état du mode sombre dans le local storage
+    if (isDarkMode) {
+      localStorage.setItem("darkMode", "enabled");
+    } else {
+      localStorage.setItem("darkMode", "disabled");
+    }
+  }
+  
+  // Vérifie l'état du mode sombre au chargement de la page
+  window.onload = function() {
+    var darkModeState = localStorage.getItem("darkMode");
+    var element = document.body;
+    
+    // Si le mode sombre était activé lors de la dernière session, activez-le à nouveau
+    if (darkModeState === "enabled") {
+      element.classList.add("dark-mode");
+    }
+  };
