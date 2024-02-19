2e7ab4106a76ff99b5156127398e56f548b4f52c
davina.houquet <davina.houquet@hotmail.com>
2024-02-19 20:32:18 +0100

    Aperçus

--- src/Repository/SessionRepository.php
+++ src/Repository/SessionRepository.php
@@ -21,108 +21,105 @@ class SessionRepository extends ServiceEntityRepository
         parent::__construct($registry, Session::class);
     }
 
-//    /**
-//     * @return Session[] Returns an array of Session objects
-//     */
-   public function findSessionsAVenir(): array
-   {
-        $now = new \DateTime();
-       return $this->createQueryBuilder('s')
-           ->andWhere('s.dateDebut > :now')
-           ->setParameter('now', $now)
-           ->orderBy('s.dateDebut', 'ASC')
-           ->getQuery()
-           ->getResult()
-       ;
-   }
-
-   public function findSessionsEnCours(): array
-   {
-        $now = new \DateTime();
-       return $this->createQueryBuilder('s')
-           ->andWhere('s.dateDebut < :now')
-           ->andWhere('s.dateFin > :now')
-           ->setParameter('now', $now)
-           ->orderBy('s.dateDebut', 'ASC')
-           ->getQuery()
-           ->getResult()
-       ;
-   }
-
-   public function findSessionsPassees(): array
-   {
-        $now = new \DateTime();
-       return $this->createQueryBuilder('s')
-           ->andWhere('s.dateFin < :now')
-           ->setParameter('now', $now)
-           ->orderBy('s.dateDebut', 'ASC')
-           ->getQuery()
-           ->getResult()
-       ;
-   }
-
-// Pour afficher les stagaires non inscrits
-   public function findNonInscrits($session_id)
-   {
-    $em = $this->getEntityManager();
-    $sub = $em->createQueryBuilder();
-
-    $qb = $sub;
-    // Query pour séléctionner tous les stagiaires d'une session dont l'id est passé en paramètres
-    $qb->select('s') //C'est un alias, on selectionne un objet ou une collection d'objets (pas des champs comme dans SQL)
-        ->from('App\Entity\Stagiaire', 's') 
-        ->leftJoin('s.sessions', 'se') 
-        ->where('se.id = :id');
-
-    $sub = $em->createQueryBuilder();
-    // Subquery pour séléctionner tous les stagiaires qui ne sont pas (NOT IN) dans le résultat précédent
-    $sub->select('st')
-        ->from('App\Entity\Stagiaire', 'st')
-        ->where($sub->expr()->notIn('st.id', $qb->getDQL()))
-        // Requête paramétrée
-        ->setParameter('id', $session_id)
-        ->orderBy('st.nom');
-
-    // Renvoyer le résultat
-    $query = $sub->getQuery();
-    return $query->getResult();
-   }
-   
-// Pour afficher les modules non programmés
-    public function findNonProgrammes($session_id)
+    //    /**
+    //     * @return Session[] Returns an array of Session objects
+    //     */
+    public function findSessionsAVenir(): array
     {
-    $em = $this->getEntityManager();
-    $sub = $em->createQueryBuilder();
-
-    $qb = $sub;
-    // Query pour séléctionner tous les modules d'un programme dont l'id est passé en paramètres (on joint programme)
-    $qb->select('m')
-        ->from('App\Entity\Module', 'm')
-        ->leftJoin('m.programmes', 'p')
-        ->leftJoin('p.session', 's')
-        ->where('s.id = :id');
-
-    $sub = $em->createQueryBuilder();
-    // Subquery pour séléctionner tous les modules qui ne sont pas (NOT IN) dans le résultat précédent
-    $sub->select('mo')
-        ->from('App\Entity\Module', 'mo')
-        ->where($sub->expr()->notIn('mo.id', $qb->getDQL()))
-
-        ->setParameter('id', $session_id);
-     
-
-    // Renvoyer le résultat
-    $query = $sub->getQuery();
-    return $query->getResult();
+        $now = new \DateTime();
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.dateDebut > :now')
+            ->setParameter('now', $now)
+            ->orderBy('s.dateDebut', 'ASC')
+            ->getQuery()
+            ->getResult();
     }
 
-//    public function findOneBySomeField($value): ?Session
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function findSessionsEnCours(): array
+    {
+        $now = new \DateTime();
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.dateDebut < :now')
+            ->andWhere('s.dateFin > :now')
+            ->setParameter('now', $now)
+            ->orderBy('s.dateDebut', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function findSessionsPassees(): array
+    {
+        $now = new \DateTime();
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.dateFin < :now')
+            ->setParameter('now', $now)
+            ->orderBy('s.dateDebut', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    // Pour afficher les stagaires non inscrits
+    public function findNonInscrits($session_id)
+    {
+        $em = $this->getEntityManager();
+        $sub = $em->createQueryBuilder();
+
+        $qb = $sub;
+        // Query pour séléctionner tous les stagiaires d'une session dont l'id est passé en paramètres
+        $qb->select('s') //C'est un alias, on selectionne un objet ou une collection d'objets (pas des champs comme dans SQL)
+            ->from('App\Entity\Stagiaire', 's')
+            ->leftJoin('s.sessions', 'se')
+            ->where('se.id = :id');
+
+        $sub = $em->createQueryBuilder();
+        // Subquery pour séléctionner tous les stagiaires qui ne sont pas (NOT IN) dans le résultat précédent
+        $sub->select('st')
+            ->from('App\Entity\Stagiaire', 'st')
+            ->where($sub->expr()->notIn('st.id', $qb->getDQL()))
+            // Requête paramétrée
+            ->setParameter('id', $session_id)
+            ->orderBy('st.nom');
+
+        // Renvoyer le résultat
+        $query = $sub->getQuery();
+        return $query->getResult();
+    }
+
+    // Pour afficher les modules non programmés
+    public function findNonProgrammes($session_id)
+    {
+        $em = $this->getEntityManager();
+        $sub = $em->createQueryBuilder();
+
+        $qb = $sub;
+        // Query pour séléctionner tous les modules d'un programme dont l'id est passé en paramètres (on joint programme)
+        $qb->select('m')
+            ->from('App\Entity\Module', 'm')
+            ->leftJoin('m.programmes', 'p')
+            ->leftJoin('p.session', 's')
+            ->where('s.id = :id');
+
+        $sub = $em->createQueryBuilder();
+        // Subquery pour séléctionner tous les modules qui ne sont pas (NOT IN) dans le résultat précédent
+        $sub->select('mo')
+            ->from('App\Entity\Module', 'mo')
+            ->where($sub->expr()->notIn('mo.id', $qb->getDQL()))
+
+            ->setParameter('id', $session_id);
+
+
+        // Renvoyer le résultat
+        $query = $sub->getQuery();
+        return $query->getResult();
+    }
+
+    //    public function findOneBySomeField($value): ?Session
+    //    {
+    //        return $this->createQueryBuilder('s')
+    //            ->andWhere('s.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
